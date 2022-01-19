9c8b80e3633bbdcfa7d598b6a146912a69915955
unknown <lorenzo.lgayle@gmail.com>
2022-01-19 10:25:38 -0500

    Task 1 done

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.eat = function(edible){
+    if(this.stomach.length <= 10){
+      this.stomach.push(edible)
+    }
+  }
+  this.poop = function(edible){
+    this.stomach = []
+  }
+  this.stomach = []
+  this.toString = function (){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 
